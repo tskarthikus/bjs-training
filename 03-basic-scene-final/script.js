b3b500ccb4b275dca7957e7c5ba855cf43af671d
Karthik <tskarthikus@gmail.com>
2023-01-11 13:38:41 +0530

    Added node modules to ignore list

--- 03-basic-scene-final/script.js
+++ 03-basic-scene-final/script.js
@@ -1,35 +1,3 @@
-// // Canvas
-// const canvas = document.querySelector('canvas.webgl')
-// const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-// // Sizes
-// const sizes = {
-//     width: 800,
-//     height: 600
-// }
-
-// // Scene
-// const scene = new THREE.Scene()
-
-// // Object
-// const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-// const cubeMaterial = new THREE.MeshBasicMaterial({
-//     color: '#ff0000'
-// })
-// const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-// scene.add(cubeMesh)
-
-// // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// camera.position.z = 3
-// scene.add(camera)
-
-// // Renderer
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: canvas
-// })
-// renderer.setSize(sizes.width, sizes.height)
-// renderer.render(scene, camera)
-
 const canvas = document.getElementById("webgl"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
