bd9d63c0fea4ed499095ba6cf03651c549739e08
Karthik <tskarthikus@gmail.com>
2023-01-20 16:34:37 +0530

    starter for today

---
+++ 15-Billboard/src/script.js
@@ -0,0 +1,81 @@
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+import './style.css'
+import * as GUI from "@babylonjs/gui";
+// import { MeshWriter } from "meshwriter";
+
+
+// Canvas
+const canvas = document.getElementById("webgl"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+const createScene = function () {
+    // Scene
+    const scene = new BABYLON.Scene(engine);
+    //TODO::
+
+   
+    // Creates a light, aiming 0,1,0 - to the sky
+    const light = new BABYLON.HemisphericLight("light", 
+        new BABYLON.Vector3(0, 1, 0), scene);
+    // Dim the light a small amount - 0 to 1
+    light.intensity = 0.7;
+
+    const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 2, 4, BABYLON.Vector3.Zero(), scene);        
+    camera.position.z = 3
+    camera.panningSensibility = 0;
+
+    camera.allowUpsideDown = false;
+    camera.lowerRadiusLimit = 3.95;
+    camera.upperRadiusLimit = 50;
+    camera.wheelPrecision = 80;
+
+
+
+    // Targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    const mesh = BABYLON.MeshBuilder.CreatePlane("plane1", {size: 2}, scene);
+    let material = new BABYLON.StandardMaterial("Box Material", scene);
+    mesh.material = material;
+    mesh.billboardMode = BABYLON.Mesh.BILLBOARDMODE_Y ;
+
+    var textureGround = new BABYLON.DynamicTexture("dynamictexture", {width:800, height:800}, scene);   
+	material.diffuseTexture = textureGround;
+
+    var torus = BABYLON.MeshBuilder.CreateTorus("torus", {thickness: 0.5, diameter: 0.8}, scene);
+    torus.position.x = -4;
+
+	var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 8, height: 4}, scene);	   
+    ground.position.y = -2;
+
+    // TODO:: <<
+    //Add text to dynamic texture
+    var font = "bold 200px monospace";
+    let text = "texttt";
+    textureGround.drawText(text, 75, 135, font, "green", "white", true, true);
+
+
+
+    return {scene};
+}
+const {scene} = createScene(); //Call the createScene function
+
+
+// Register a render loop to repeatedly render the scene
+/**
+ * Animate
+ */
+let time = Date.now()
+engine.runRenderLoop(function () {
+    scene.render();
+});
+
+const onResize = () => {
+    canvas.width = getWidth();
+    engine.resize();
+}
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", onResize);
---
+++ 16-Shadow-starter/src/script.js
@@ -0,0 +1,73 @@
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+import './style.css'
+
+
+// Canvas
+const canvas = document.getElementById("webgl"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+const createScene = function () {
+	var scene = new BABYLON.Scene(engine);
+	
+	//TODO::
+    scene.clearColor = new BABYLON.Color4(0.2, 0.3, 0.2, 1);
+
+	// Setup environment
+	var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 90, BABYLON.Vector3.Zero(), scene);
+	camera.lowerBetaLimit = 0.1;
+	camera.upperBetaLimit = (Math.PI / 2) * 0.9;
+	camera.lowerRadiusLimit = 30;
+	camera.upperRadiusLimit = 150;
+	camera.attachControl(canvas, true);
+
+	// light1
+	var light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(-1, -2, -1), scene);
+	light.position = new BABYLON.Vector3(20, 40, 20);
+	light.intensity = 0.5;
+
+	// Ground
+	var ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", "heightMap.png", 
+		{
+			width: 100, 
+			height: 100, 
+			subdivisions: 50, 
+			minHeight: 0, 
+			maxHeight: 10
+		}, 
+		scene, false);
+		
+	var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+	groundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
+	groundMaterial.diffuseTexture.uScale = 6;
+	groundMaterial.diffuseTexture.vScale = 6;
+	groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+	ground.position.y = -2.05;
+	ground.material = groundMaterial;
+
+	//TODO:: 
+
+
+
+	// Animations
+	scene.registerBeforeRender(function () {
+	});
+
+    return {scene};
+}
+const {scene} = createScene(); //Call the createScene function
+
+
+// Register a render loop to repeatedly render the scene
+
+engine.runRenderLoop(function () {
+
+    scene.render();
+});
+
+const onResize = () => {
+    canvas.width = getWidth();
+    engine.resize();
+}
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", onResize);
